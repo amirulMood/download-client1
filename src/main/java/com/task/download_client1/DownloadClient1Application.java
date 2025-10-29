5638548dea0753c9070318c3bae99cd48e5d20a0
amiruldanielcs <106524830+amirulMood@users.noreply.github.com>
2025-10-29 22:08:21 +0800

    Initial commit

---
+++ src/main/java/com/task/download_client1/DownloadClient1Application.java
@@ -0,0 +1,13 @@
+package com.task.download_client1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadClient1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/download_client1/DownloadClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.download_client1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadClient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
